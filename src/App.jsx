65d0f13059ad970e87b42ef0e36d0083a0230aef
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2023-12-28 18:25:59 +0000

    changes are made

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Vineet Agarwal",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "vineetagarwal.now@gmail.com",
+  gitHub: "vineeTagarwaL-code",
+  instagram: "codewithvineet", 
+  linkedIn: "vineetagarwal2004",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "vineetwts",
+  youTube: "codewithvineet",
 };
 
 const primaryColor = "#4E567E";
